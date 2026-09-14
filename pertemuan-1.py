8d347d4b4795d0b358b4ba3b2a9ded80cc63b242
Hafizh-Firdaus-L10n <hafizh.firdaus.l10n@gmail.com>
2026-09-14 12:13:40 +0800

    first commit

---
+++ pertemuan-1.py
@@ -0,0 +1,50 @@
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+
+# a = 5.5
+# b = 10.10
+# c = a + b
+# print(c)
+
+# print('''Halo saya Rendy
+# Saya suka belajar coding
+# Hari ini saya belajar bahasa pemrograman Python''')
+
+# teks = "Rahmat Flowchart"
+
+# # print(teks[3])
+# # #menampilkan index ke-3 hingga ke-6
+# # print(teks[3:6])
+# # #menampilkan seluruhnya, tetapi dimulai dari index ke-7
+# # print(teks[7:])
+# # #menampilkan hanya sampai index ke-6
+# # print(teks[:6])
+
+# mata_kuliah = ["APD", "Logika Matematika", "Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])
+
+# data_mahasiswa = ("Rafi", "034", True, 3.5)
+# print(data_mahasiswa[0])
+# print(data_mahasiswa[2])
+
+# buku = {"judul": "Atomic Habits", "penulis": "James Clear", "halaman": 320}
+
+# # Mengakses Value dalam Dictionary
+# print(buku["judul"])
+# print(buku["penulis"])
+# print(buku["halaman"])
+
+# a = "20"
+# print(type(a))
+
+# nama = input("Masukan nama anda: ")
+# print(nama)
+
+a = 20
+b = 30
+c = a * b
+print(c)
