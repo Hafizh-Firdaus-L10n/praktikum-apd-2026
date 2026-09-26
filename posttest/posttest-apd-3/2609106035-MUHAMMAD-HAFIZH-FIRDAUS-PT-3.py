33a0a1c1300392d31f7db1db08ae429aa46c2f41
Hafizh-Firdaus-L10n <hafizh.firdaus.l10n@gmail.com>
2026-09-26 21:03:26 +0800

    upload file python posttest 3 apd

--- posttest/posttest-apd-3/2609106035-MUHAMMAD-HAFIZH-FIRDAUS-PT-3.py
+++ posttest/posttest-apd-3/2609106035-MUHAMMAD-HAFIZH-FIRDAUS-PT-3.py
@@ -1,7 +1,7 @@
 nama = input("Silahkan masukkan nama anda: ")
 nim = input("Silahkan masukkan NIM anda: ")
 
-if nama == "hafizh" and nim == "35":
+if nama.lower() == "hafizh" and nim == "35":
     print("Selamat datang, Hafizh.")
 
     pertalite = 10000
@@ -9,9 +9,10 @@ if nama == "hafizh" and nim == "35":
     pertamax_turbo = 15000
 
     print("\nJenis Bensin:")
-    print("1. Pertalite: Rp. ", pertalite)
-    print("2. Pertamax: Rp. ", pertamax)
-    print("3. Pertamax Turbo Rp.: ", pertamax_turbo)
+    print("1. Pertalite      : Rp", pertalite)
+    print("2. Pertamax       : Rp", pertamax)
+    print("3. Pertamax Turbo : Rp", pertamax_turbo)
+
     pilihan_bensin = input("Pilih jenis bensin (1-3): ")
 
     if pilihan_bensin == "1":
@@ -24,44 +25,45 @@ if nama == "hafizh" and nim == "35":
         jenis = "Pertamax Turbo"
         harga_per_liter = pertamax_turbo
     else:
-        print("Pilihan tidak valid.")
+        jenis = None
+        harga_per_liter = 0
 
-    jumlah_liter = float(input("Masukkan jumlah liter: "))
+    if jenis is not None:
+        jumlah_liter = float(input("Masukkan jumlah liter: "))
 
-    total_harga = harga_per_liter * jumlah_liter
+        total_harga = harga_per_liter * jumlah_liter
 
-    if jumlah_liter >= 10:
-        diskon_pembelian = 0.10 * total_harga
-    elif jumlah_liter >= 5:
-        diskon_pembelian = 0.05 * total_harga
+        if jumlah_liter >= 10:
+            diskon_pembelian = 0.10 * total_harga
+        elif jumlah_liter >= 5:
+            diskon_pembelian = 0.05 * total_harga
+        else:
+            diskon_pembelian = 0
+
+        member = input("Apakah pembeli member? (ya/tidak): ").lower()
+        if member == "ya":
+            diskon_member = 0.02 * total_harga
+        else:
+            diskon_member = 0
+
+        total_diskon = diskon_pembelian + diskon_member
+        total_bayar = total_harga - total_diskon
+
+        print("\n==============================================")
+        print("             STRUK TRANSAKSI SPBU             ")
+        print("==============================================")
+        print("Nama Pembeli       :", nama)
+        print("NIM Pembeli        :", nim)
+        print("Jenis BBM          :", jenis)
+        print("Jumlah Liter       :", jumlah_liter, "L")
+        print("Total Harga        : Rp", int(total_harga))
+        print("Diskon Pembelian   : Rp", int(diskon_pembelian))
+        print("Diskon Member      : Rp", int(diskon_member))
+        print("----------------------------------------------")
+        print("TOTAL BAYAR        : Rp", int(total_bayar))
+        print("==============================================")
     else:
-        diskon_pembelian = 0
-
-    print("Total harga:", total_harga)
-    print("Diskon:", diskon_pembelian)
-    print("Total bayar:", total_harga - diskon_pembelian)
-
-    member = input("Apakah pembeli member? (ya/tidak)")
-    if member == "ya":
-        diskon_member = 0.02 * total_harga
-    else:
-        diskon_member = 0
-
-    total_diskon = diskon_pembelian + diskon_member
-    total_bayar = total_harga - total_diskon
-
-    print("\n==============================================")
-    print("              STRUK TRANSAKSI SPBU             ")
-    print("==============================================")
-    print("Nama Pembeli       :", nama)
-    print("NIM Pembeli        :", nim)
-    print("Jenis BBM          :", jenis)
-    print("Jumlah Liter       :", jumlah_liter, "L")
-    print("Total Harga        : Rp", total_harga)
-    print("Diskon             : Rp", diskon_pembelian + diskon_member)
-    print("----------------------------------------------")
-    print("TOTAL BAYAR        : Rp", total_bayar)
-    print("==============================================")
+        print("Pilihan jenis bensin tidak valid. Program dihentikan.")
 
 else:
     print("Login salah. Nama atau NIM salah.")
